9875a121360a6db8b061e51d04a0a32434c179e9
amirasalah <amera.salah.89@gmail.com>
2020-11-13 22:49:51 +0200

    getting data

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-html
+const URL = 'https://dog.ceo/api/breeds/image/random'
+
+
+function fetchRandomDogs(url) {
+    let data = []
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            data = json
+            console.log(data.message)
+            return data.message
+        })
+}
+
+fetchRandomDogs(URL)
