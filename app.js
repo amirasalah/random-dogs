5f7c01a96ec6ee26585d6ed57bb4a828d5ebbe9a
amirasalah <amera.salah.89@gmail.com>
2020-11-14 12:09:41 +0200

    set interval

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const URL = 'https://dog.ceo/api/breeds/image/random'
 
 const image = document.querySelector('.image')
+const innerImage = document.createElement('img')
 
 function fetchRandomDogs(url) {
     let imageUrl = ''
+    innerImage.innerHTML = ''
     fetch(url)
         .then(response => response.json())
         .then(json => {
             imageUrl = json.message
-            const innerImage = document.createElement('img')
             innerImage.src = imageUrl
             image.appendChild(innerImage)
         })
 }
 fetchRandomDogs(URL)
+setInterval(() => {
+    fetchRandomDogs(URL)
+}, 10000)
